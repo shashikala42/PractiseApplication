87fc776388d747cef421102acd539a42ac84786c
ADMIN <ADMIN@DESKTOP-5I6EJ48>
2025-01-22 10:30:58 +0530

    update - correction to program.cs file

--- PractiseApp/Program.cs
+++ PractiseApp/Program.cs
@@ -1,18 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PractiseApp;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Rayan!");
 Logic logic = new Logic();
-//var result = logic.reverseDigits(457);
-//var longestSubstring = logic.longestSubstringWithoutRepeatingCharacter("abcabdabfg");
-//Console.WriteLine("Result", longestSubstring);
-//var longestPalindrome = logic.LongestPalindromicSubstring("geeksskeegabc");
-//var checkPalindrome = logic.CheckPalindromeNumber(121);
-//Console.WriteLine("Result", checkPalindrome);
-//var stringtoInteger = logic.ConvertStringtoInteger("123shash12i");
-//var calculateMaxWidth = logic.CalculateMaximumWidthfromLines();
-//var convertRomantoInteger = logic.ConvertRomantoInteger("1254367");
+var resultReverseDigits = logic.reverseDigits(457);
+Console.WriteLine("resultReverseDigits", resultReverseDigits);
+var longestSubstring = logic.longestSubstringWithoutRepeatingCharacter("abcabdabfg");
+Console.WriteLine("longestSubstring", longestSubstring);
+var longestPalindrome = logic.LongestPalindromicSubstring("geeksskeegabc");
+Console.WriteLine("longestPalindrome", longestPalindrome);
+var checkPalindrome = logic.CheckPalindromeNumber(121);
+Console.WriteLine("checkPalindrome", checkPalindrome);
+var stringtoInteger = logic.ConvertStringtoInteger("123shash12i");
+Console.WriteLine("stringtoInteger", stringtoInteger);
+var calculateMaxWidth = logic.CalculateMaximumWidthfromLines();
+Console.WriteLine("calculateMaxWidth", calculateMaxWidth);
+var convertRomantoInteger = logic.ConvertRomantoInteger("1254367");
+Console.WriteLine("convertRomantoInteger", convertRomantoInteger);
 int[] array = { 10, 20, 4, 45, 99, 45 };
 string[] strs = { "flower", "flow", "flight" };
-//var findSecondLargest = logic.FindSecondLargestNumber(array);
+var findSecondLargest = logic.FindSecondLargestNumber(array);
+Console.WriteLine("findSecondLargest", findSecondLargest);
 var findCommonPrefixInListofstring = logic.LongestCommonPrefix(strs);
+Console.WriteLine("findCommonPrefixInListofstring", findCommonPrefixInListofstring);
